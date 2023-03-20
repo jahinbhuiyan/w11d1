e5af470df448fe10eec220aa6e22d918f0d9db8f
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-20 14:29:32 -0400

    phase 1 ongoing

---
+++ form_project/src/App.js
@@ -0,0 +1,12 @@
+import Form from "./form";
+
+function App() {
+  return (
+    <>
+    <h1>Hello from App</h1>
+    <Form/>
+    </>
+  );
+}
+
+export default App;
---
+++ form_project/src/form.jsx
@@ -0,0 +1,34 @@
+
+
+const Form = () => {
+
+
+    return(
+        <>
+        <form>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email"/>
+            <input type="text" placeholder="PhoneNumber"/>
+            <select name="PhoneType">
+                <option value="home"> Home </option>
+                <option value="work"> Work </option>
+                <option value="mobile"> Mobile </option>
+            </select>
+            <input type="radio" id="instructor" name="staff" value="instructor"/>
+            <label for="instructor">Instructor</label>
+            <input type="radio" id="student" name="staff" value="student"/>
+            <label for="student">Student</label>
+            <br/>
+            <label for="bio">Biography</label>
+            <br/>
+            <textarea id="bio" name="bio" placeholder="enter BIO here"/>
+            <br/>
+            <input type="checkbox" id="email" name="email"/>
+            <label for="email">Email Notification?</label>
+        </form>
+        </>
+    )
+
+}
+
+export default Form;
