50e9c82e6dc23c244191eec368d3b2e433cae621
Kaiter Wu <wukaiter@gmail.com>
2023-03-20 15:06:56 -0400

    creating onchange function and filling out skeleton

--- form_project/src/form.jsx
+++ form_project/src/form.jsx
@@ -1,14 +1,79 @@
-
+import {useState} from 'react';
 
 const Form = () => {
+
+    const [user,setUser] = useState({
+        name: "",
+        email: "",
+        phoneNumber: "",
+        phoneType: "", 
+        staff: "",
+        bio: "",
+        emailNotifications: ""
+    });
+
+    const [errors,setErrors] = useState([]);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        const currentErrors = validate();
+        setErrors(currentErrors);
+
+        if (currentErrors.length){
+            console.log("errors!");
+        }else{
+            console.log("Making fake requests")
+        }
+    }
+
+    const handleChange = (field)=>{
+        // debugger
+        return (e)=>{
+            const val = e.target.value;
+
+            setUser({
+                ...user,
+                [field]:val
+            });
+        };
+    };
+
+    const validate = () =>{
+        const currentErrors = [];
+
+        if (user.name.length ===0){
+            currentErrors.push("Name cannot be empty")
+        }
+
+        if (user.email.length ===0 ){
+            currentErrors.push("Email cannot be empty")
+        }
+         if (user.phoneNumber.length){
+            if(user.phoneNumber.length !== 12){
+                currentErrors.push("Not a valid phonenumber")
+            }
+            if(!user.phoneType){
+                currentErrors.push("Please select a phone type!")
+            }
+         }
+
+         if (user.bio.length > 280){
+            currentErrors.push("Exceeded character limit")
+         }
+        }
+    
 
 
     return(
         <>
-        <form>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email"/>
-            <input type="number" placeholder="PhoneNumber"/>
+        <ul>
+            {errors.map((error,i) => <li key = {i}>{error}</li>)}
+        </ul>
+        <form onSubmit = {handleSubmit}>
+            <input type="text" placeholder="Name" value = {user.name} onChange={handleChange("name")} />
+            <input type="email" placeholder="Email" value = {user.email} onChange = {handleChange("email")}/>
+            <input type="number" placeholder="1-###-###-####" value = {user.phoneNumber} onChange ={handleChange("phoneNumber")}/>
             <select name="PhoneType">
                 <option value="home"> Home </option>
                 <option value="work"> Work </option>
@@ -25,6 +90,8 @@ const Form = () => {
             <br/>
             <input type="checkbox" id="email" name="email"/>
             <label for="email">Email Notification?</label>
+            <br/>
+            <input type = "submit" value = "Submit"/>
         </form>
         </>
     )
